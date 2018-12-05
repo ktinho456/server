cde76b3471b964e600b965d5c00d8bcbc1243ad9
ktinho456 <ktinho456@users.noreply.github.com>
2018-12-05 19:38:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  
+    ///io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+ /// });
+});
+  
+  
+  
+  
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
@@ -63,11 +73,7 @@ io.sockets.on('connection',
         });
   
   
-  io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+
   
   
   
